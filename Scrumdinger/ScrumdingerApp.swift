e2b8f7bb861ea501b59847a6ecae9215687b8c64
Nikhil Sharma <sharmanik99@berkeley.edu>
2021-12-25 14:07:34 -0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Nikhil Sharma on 12/25/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
